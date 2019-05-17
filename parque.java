360d3e99351bb9f7d46b2cd506a24a613ac24732
oranz <corun@Ada-Byron>
2019-05-17 21:18:39 +0200

    Se añaden constantes. Se añade constructor con las atracciones iniciales Se añade declaración de método para generar datos desde un repositorio.

--- parque.java
+++ parque.java
@@ -6,6 +6,7 @@
 
 
 import park.atracciones.ListaAtracciones;
+import park.atracciones.TipoAtraccion;
 import park.empleados.ListaEmpleados;
 import park.entradas.ListaEntradas;
 import park.usuarios.ListaUsuarios;
@@ -18,61 +19,64 @@ import park.ventas.Venta;
  * @author corun
  */
 public class parque {
-    
+    // Listas estáticas
     public static final ListaEntradas listaEntradas = ListaEntradas.getListaEntradas();
     public static final ListaAtracciones listaAtracciones = ListaAtracciones.getListaAtracciones();
     public static final ListaEmpleados listaEmpleados = ListaEmpleados.getListaEmpleados();
     public static final ListaVentas listaVentas = ListaVentas.getListaVentas();
     public static final ListaUsuarios listaUsuarios = ListaUsuarios.getListaUsuarios();
     
+    // Constantes
+    private final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_A=4;
+    private final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_B=6;
+    private final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_C=4;
+    private final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_D=3;
+    private final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_E=7;
+    
+    // Constanes de mensajes
+    private final String CONSTRUYENDO_PARQUE = "Iniciando la construcción del parque:\n"
+                                            + "- Construyendo atracciones...\n";
+    private final String ATRACCIONES_CONSTRUIDAS = "\tSe han construido las atracciones...\n"
+                                            + "- Contratando empleados...";
+    private final String EMPLEADOS_CONTRATADOS = "\tSe han contratado a los empleados...\n"
+                                            + "- Ultimando apertura...\n";
+    private final String PARQUE_INAUGURADO = "El parque ya está funcionando.\n";
+    private final static String FIN_PROGRAMA = "Fin de programa";
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Constantes locales
-//        final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_A=4;
-//        final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_B=6;
-//        final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_C=4;
-//        final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_D=3;
-//        final int CANTIDAD_ATRACCIONES_INICIALES_TIPO_E=7;
-//        
-//        final String TEXTO_INICIALIZANDO_APLICACION = "Inicializando la aplicación Parque de atracciones.\n"
-//                + "-------------------------------------------------";
-//        final String TEXTO_CREACION_ATRACCIONES_INICIALES = "Se inicia la creación de"
-//                + " atracciones requeridas (%d tipo A, %d tipo B, %d tipo C, %d tipo D"
-//                + "(sobreentiendo que hay una errata en el enunciado) y %d tipo E.\n";
-//        final String TEXTO_FIN_CREACION_ATRACCIONES_INICIALES = "Se han creado las atracciones.";
-//        final String TEXTO_RECORDATORIO_ACTIVACION_PARQUES = "Se debe proceder a la activación de los parques y"
-//                + " contratación de los empleados para iniciar la actividad del parque.";
-//        
-//        // Variables locales
-//        int ayudantesNecesarios, responsablesNecesarios, atencionClientesNecesarios, relacionesNecesarios;
-//        
-//        // Inicio de las acciones de carga de la aplicación
-//        // Mensaje bienvenida
-//        System.out.println(TEXTO_INICIALIZANDO_APLICACION);
-//        
-//        // Inicio de la creación de las atracciones inciales
-//        System.out.printf(TEXTO_CREACION_ATRACCIONES_INICIALES,
-//                CANTIDAD_ATRACCIONES_INICIALES_TIPO_A,
-//                CANTIDAD_ATRACCIONES_INICIALES_TIPO_B,
-//                CANTIDAD_ATRACCIONES_INICIALES_TIPO_C,
-//                CANTIDAD_ATRACCIONES_INICIALES_TIPO_D,
-//                CANTIDAD_ATRACCIONES_INICIALES_TIPO_E);
-//        
-//        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_A,TipoAtraccion.A);
-//        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_B,TipoAtraccion.B);
-//        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_C,TipoAtraccion.C);
-//        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_D,TipoAtraccion.D);
-//        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_E,TipoAtraccion.E);
-//        
-//        System.out.println(TEXTO_FIN_CREACION_ATRACCIONES_INICIALES);
-//        
-//        // Inicio de la creación de empleados necesario para el inicio del parque.
-//        System.out.println(TEXTO_RECORDATORIO_ACTIVACION_PARQUES);
-//        
-//       
-//        System.out.println(ListaEntradas.getListaEntradas().isEmpty());
+           parque parque1 = new parque();
+           System.out.print(FIN_PROGRAMA);
+    }
+    
+    /**
+     * En el constructor del parque se inicia con las atracciones indicadas en el ejercicio
+     * a fecha 1/enero/2019 (fecha en la que decido iniciar el parque) con todos los empleados
+     * necesarios para su puesta en marcha.
+     */
+    public parque(){
+        
+        System.out.print(CONSTRUYENDO_PARQUE);
+        
+        // Se añaden las atracciones iniciales indicadas en el punto 2 de la práctica
+        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_A,TipoAtraccion.A);
+        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_B,TipoAtraccion.B);
+        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_C,TipoAtraccion.C);
+        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_D,TipoAtraccion.D);
+        ListaAtracciones.crearAtracciones(CANTIDAD_ATRACCIONES_INICIALES_TIPO_E,TipoAtraccion.E);
+        
+        System.out.print(ATRACCIONES_CONSTRUIDAS);
+        
+        // Dado que en el mismo punto se indica el porcentaje de empleados necesarios,
+        // se presupone que están todas las atracciones activas en el inicio del parque.
+        
+        
+        System.out.print(EMPLEADOS_CONTRATADOS);
+        
+        System.out.print(PARQUE_INAUGURADO);
+        
     }
     
     public Venta comprarEntradaTaquilla(boolean complementoVIP){  
@@ -100,5 +104,12 @@ public class parque {
     
     public ListaUsuarios getListaUsuarios(){
         return parque.listaUsuarios;
+    }  
+    
+    /**
+     * Método que creará datos simulados del parque.
+     */
+    public void runRepositorioDatos(){
+        
     }
 }
